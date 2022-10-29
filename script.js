140ff568d6c94bf9d64af28cab68e289130c868b
kolexstrings <66323820+kolexstrings@users.noreply.github.com>
2022-10-29 05:35:39 +0100

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let selection = document.querySelector("select");
 let result = document.querySelector("h2");
 
-selection.addEventListener ("change", () => {
+selection.onchange = () => {
     result.innerText = selection.options[selection.selectedIndex].text;
-});
+};
